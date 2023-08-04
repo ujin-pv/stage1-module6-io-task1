e719f7c8d4867a067d10444ef9c85cc75818b474
Eugen <ujin.pavlenko@gmail.com>
2023-08-04 22:04:54 +0300

    comm 1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,32 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            String[] data = new String[]{"", "", "", ""};
+            int c;
+            boolean flg = false;
+            int it = 0;
+            while ((c = inputStream.read()) != -1) {
+                if (c == '\r') {
+                    flg = false;
+                    it++;
+                }
+                if (flg) data[it] += (char) c;
+                if (c == ' ') flg = true;
+            }
+            return new Profile(data[0], Integer.parseInt(data[1]), data[2], Long.parseLong(data[3]));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
+
 }
