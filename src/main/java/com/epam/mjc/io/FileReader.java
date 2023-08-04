3e42729068b368a7f263f14cdeb8741fec493b63
Eugen <ujin.pavlenko@gmail.com>
2023-08-04 22:17:28 +0300

    comm 2

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -9,24 +9,30 @@ public class FileReader {
 
     public Profile getDataFromFile(File file) {
 
-        try (FileInputStream inputStream = new FileInputStream(file)) {
-            String[] data = new String[]{"", "", "", ""};
+        try (FileInputStream inputStream = new FileInputStream(file)){
+            String[] data = new String[] {"", "", "", ""};
             int c;
             boolean flg = false;
             int it = 0;
             while ((c = inputStream.read()) != -1) {
-                if (c == '\r') {
+                if(c == '\r'){
                     flg = false;
                     it++;
                 }
-                if (flg) data[it] += (char) c;
-                if (c == ' ') flg = true;
+                if(flg) data[it] += (char)c;
+                if(c == ' ') flg = true;
             }
-            return new Profile(data[0], Integer.parseInt(data[1]), data[2], Long.parseLong(data[3]));
+            if (it == 4)
+                return new Profile(data[0], Integer.parseInt(data[1]), data[2], Long.parseLong(data[3]));
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
         }
+        return null;
     }
 
+    public static void main(String[] args){
+        FileReader fileReader = new FileReader();
+        Profile actual = fileReader.getDataFromFile(new File("src/main/resources/Profile.txt"));
+
+    }
 }
